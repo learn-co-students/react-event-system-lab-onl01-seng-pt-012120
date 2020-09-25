b163d35be4e9f7d6eaa1c79c2f71d7fcf5d9709b
LaNice Powell <lanicemp@aol.com>
2020-09-25 19:21:40 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+
+export default class EyesOnMe extends React.Component{
+    constructor(props){
+        super()
+    }
+
+    blur(event){
+        console.log('Hey! Eyes on me!')
+    }
+
+    focus(event){
+        console.log('Good!')
+    }
+render(){
+    return (
+        <button onFocus={this.focus} onBlur={this.blur}>button</button>
+    )
+}
+}
